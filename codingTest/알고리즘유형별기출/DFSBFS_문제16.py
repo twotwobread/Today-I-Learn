319306890c3acd87f7d7a7318f1ab0b9e5be10fc
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-02-09 20:23:26 +0900

    Update DFSBFS_문제16.py

--- codingTest/알고리즘유형별기출/DFSBFS_문제16.py
+++ codingTest/알고리즘유형별기출/DFSBFS_문제16.py
@@ -8,33 +8,14 @@
 # 3 <= N,M <= 8 ( 세로크기(행), 가로 크기(열) )
 # 2의 개수는 2보다 크거나 같고, 10보다 작거나 같은 자연수입니다.
 # 빈칸의 개수는 3개 이상입니다.
+# 2초, 512MB
 # author : Lee Suyoung (2022-02-08)
 
-# 잠만 이거 조합이용봐야겠다.
-
-def dfs(row, col, visited, sum):
-    if row<0 or row>=n or col<0 or col>=m or graph[row][col] == 1 or visited[row][col] == True:
-        return sum
-    if graph[row][col] == 2:
-        return -1
-    elif graph[row][col] == 0:
-        visited[row][col] = True
-        direct1 = dfs(row+1, col, visited, sum+1)
-        if direct1 == -1:
-            return -1
-        direct2 = dfs(row-1, col, visited, direct1)
-        if direct2 == -1:
-            return -1
-        direct3 = dfs(row, col+1, visited, direct2)
-        if direct3 == -1:
-            return -1
-        direct4 = dfs(row, col-1, visited, direct3)
-        if direct4 == -1:
-            return -1
-        return direct4
+# 이거 조합이용봐야겠다.    
 
 from itertools import *
 import sys
+
 input = sys.stdin.readline
 INF = int(1e9)
 
@@ -50,15 +31,29 @@ for i in range(n):
         if graph[i][j] == 0:
             case.append((i,j))
 
+def dfs(graph, row, col, visited, sum, virusCheck):
+    if row<0 or row>=n or col<0 or col>=m or graph[row][col]==1:
+        return virusCheck, sum
+    if not visited[row][col]:
+        if graph[row][col] == 2:
+            virusCheck = True
+        visited[row][col] = True
+        sum += 1
+        virusCheck, sum = dfs(graph, row+1, col, visited, sum, virusCheck)
+        virusCheck, sum = dfs(graph, row, col+1, visited, sum, virusCheck)
+        virusCheck, sum = dfs(graph, row-1, col, visited, sum, virusCheck)
+        virusCheck, sum = dfs(graph, row, col-1, visited, sum, virusCheck)
+        return virusCheck, sum
+    return virusCheck, sum
+        
+# 모든 조합 만들기
 _case = list(combinations(case, 3))
-print(_case)
 safeCount = 0
-
-for c in _case:
+for c in _case: # 조합의 모든 경우 수 
     # 방문정보 확인하기 위한 2차원 리스트
-    visited = [[False]*m for i in range(n)]
-    temp=[]
-    x, y, z = c 
+    visited = [[False]*m for i in range(n)] #
+    temp = []
+    x, y, z = c
     temp.append(graph[x[0]][x[1]])
     temp.append(graph[y[0]][y[1]])
     temp.append(graph[z[0]][z[1]])
@@ -66,24 +61,38 @@ for c in _case:
     graph[x[0]][x[1]] = 1
     graph[y[0]][y[1]] = 1
     graph[z[0]][z[1]] = 1
-
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 2:
-                num = dfs(i,j,visited, 0)
-                if num == -1:
-                    continue
-                cnt += num
-    if safeCount < cnt:
-        best = c
-        safeCount = cnt
     
+    tempCount = 0
+    for row in range(n):
+        for col in range(m):
+            virusCheck = False
+            sum = 0
+            if graph[row][col] == 0 and not visited[row][col]:
+                virusCheck, sum = dfs(graph, row, col, visited, 0, virusCheck)
+                if virusCheck == True:
+                    continue
+                tempCount += sum
+
+    if safeCount < tempCount:
+        safeCount = tempCount
+        bestPoint =  c
+
     graph[x[0]][x[1]] = temp[0]
     graph[y[0]][y[1]] = temp[1]
     graph[z[0]][z[1]] = temp[2]
+
 print(safeCount)
-print(best)
+
 
 ## 아 조합이용하는 거 맞는거 같은데 dfs하는 부분을 다시 생각을 정리해서 풀어야할것 같다. ㅠㅠ
 # 내일 다시 풀자 !!
+# 2022-02-09 풀었다. 조합을 이용해서 3개 아무거나 뽑아서 그중 가장 작은 값을 출력한다.
+# 2를 만나면 이제 virusCheck를 True로 바꾸어 더하지 않도록 만들었다.
+# 원래 deepcopy를 이용해서 graph를 계속 복사하면서 돌렸는데 그렇게 하면 시간이 더 오래걸릴것이라
+# 예상하여 graph의 값을 계속바꾸면서 반복했다.
+# 근데 이게 2초인데 시간이 3680ms = 3.68초가 걸렸다. 근데 통과가 되었다.
+# 일단 계산을 해보면 최대 8x8의 판이고 2를 최저로 2개 그거말고 다 빈칸이라 가정.
+# 62개 중에서 3개를 뽑아야한다 => 37,820개의 가짓수가 있다. 이걸 하나할때마다. for문은 64번이 돌고
+# 거기에 dfs가 최대한 들어간다면 61번이라고 생각-> 재귀라서 다시 나온다. 122번
+# 대략적으로 122x1+61*37,820 = 2,420,480 + 122 정도인데 재귀의 오버헤드가 굉장히 많이 잡아먹는 것 같다.
+ 
