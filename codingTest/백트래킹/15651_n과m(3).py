93573ddc22b5d881ce27d43d6bd45d19d44735a4
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-03-11 23:15:11 +0900

    Create 15651_n과m(3).py

---
+++ codingTest/백트래킹/15651_n과m(3).py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split(" "))
+
+result = []
+whole = []
+
+def dfs(i):
+    if len(result) == m: 
+        print(str(" ".join(result)))
+        return
+    for i in range(1, n+1):
+        result.append(str(i))
+        dfs(i)
+        result.pop()
+for i in range(1, n+1):
+    result.append(str(i))
+    dfs(i)
+    result.pop()
