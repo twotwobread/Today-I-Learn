c0be6f13368594acb4e7326f64d52cf8c7a5058f
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-03-28 07:27:38 +0900

    Update 17144_미세먼지안녕.py

--- codingTest/스터디/삼성/17144_미세먼지안녕.py
+++ codingTest/스터디/삼성/17144_미세먼지안녕.py
@@ -35,15 +35,16 @@ def solution(r,c,t,A):
     muchine = [] # 먼저 들어가는 놈이 위쪽이고 나중에 아래쪽
     for i in range(r):
         for j in range(c):
-            if A[r][c] == -1:
-                muchine.append((r,c))
-            elif A[r][c] != 0:
-                dusk.append((r,c,A[r][c])) # 행, 열, 먼지양
+            if A[i][j] == -1:
+                muchine.append((i,j))
+            elif A[i][j] != 0:
+                dusk.append((i,j,A[i][j])) # 행, 열, 먼지양
     
     time = 0
     dx = (1,-1,0,0)
     dy = (0,0,1,-1)
     while time < t: # t초 후 종료
+        time += 1
         # 1. 미세먼지가 확산. 확산은 모든 칸에서 동시에 일어남.
         while dusk: 
             row, col, amount = dusk.popleft()
@@ -53,36 +54,96 @@ def solution(r,c,t,A):
                 if 0<=adj_x<r and 0<=adj_y<c and A[adj_x][adj_y] != -1:
                     A[adj_x][adj_y] += diffu
                     A[row][col] -= diffu
-        # 2. 공기청정기 작동.
+        print()
+        print(t, "초 전의 상태 : ")  
         for i in range(r):
             for j in range(c):
-                if A[r][c] != 0 and A[r][c] != -1:
+                print(A[i][j], end=" ")
+                #if A[i][j] != -1 and A[i][j] != 0 and not (i, j, A[i][j]) in dusk:
+                #    print(i, j, A[i][j], "가 안들어있음.")
+            print()
+        print()
+        # 2. 공기청정기 작동.
+        count = 0
+        check = [-1,-1]
+        for i in range(r):
+            temp = -1
+            for j in range(c):
+                value = -1
+                if check[0] != -1 and check[1] != -1 and i == check[0] and j == check[1]:
+                    check[0] = -1
+                    check[1] = -1
+                    continue
+                if temp != -1:
+                    if temp[0] == i and temp[1] == j:
+                        value = temp[2]
+                        temp = -1
+                if A[i][j] != 0 and A[i][j] != -1:
+                    if value == -1:
+                        count += A[i][j]
                     # 위쪽 혹은 아래쪽 공기청정기랑 행이 같은 경우
                     if (i == muchine[0][0] or i == muchine[1][0]) and j != (c-1):
                         next_x, next_y = i, j+1
-                        pass
+                        if A[next_x][next_y] == 0:
+                            check[0] = next_x
+                            check[1] = next_y
+                        
                     # 맨 위 행이거나 맨 아래 행인 경우
                     elif (i == (r-1) or i == 0) and j != muchine[0][1]: 
                         next_x, next_y = i, j-1
-                        pass
+                        
                     # 공기청정기랑 열이 같고 위쪽 행일 때
                     elif j == muchine[0][1] and i < muchine[0][0]:
                         next_x, next_y = i+1, j
-                        # 공기청정기에 먼지 먹히는 걸 생각
-                        pass
+                        if A[next_x][next_y] == 0:
+                            check[0] = next_x
+                            check[1] = next_y
+                        
                     # 공기청정기랑 열이 같고 아래쪽 행일 때
                     elif j == muchine[1][1] and i > muchine[1][0]:
                         next_x, next_y = i-1, j
-                        # 공기청정기에 먼지 먹히는 걸 생각
-                        pass
+                        
                     # 열이 마지막 열이고 위쪽 행일 때
-                    elif j == (c-1) and i <= muchine[0][0]:
+                    elif j == (c-1) and i <= muchine[0][0] and i != 0:
                         next_x, next_y = i-1, j
-                        pass
+                        
                     # 열이 마지막 열이고 아래쪽 행일 때
-                    elif j == (c-1) and i >= muchine[1][0]:
+                    elif j == (c-1) and i >= muchine[1][0] and i != (r-1):
                         next_x, next_y = i+1, j
-                        pass
+                        if A[next_x][next_y] == 0:
+                            check[0] = next_x
+                            check[1] = next_y
+                    else: # 아무것도 아닌 경우.
+                        dusk.append((i,j, A[i][j]))
+                        
+                    if 0<=next_x<r and 0<=next_y<c:
+                        if A[next_x][next_y] == -1:
+                            count -= A[i][j]
+                            A[i][j] = 0
+                            continue
+
+                        if A[next_x][next_y] != 0:
+                            temp = (next_x, next_y, A[next_x][next_y])
+                        if value != -1:
+                            A[next_x][next_y] = value
+                            dusk.append((next_x, next_y, value))
+                            count += value
+                        else:
+                            A[next_x][next_y] = A[i][j]
+                            dusk.append((next_x, next_y, A[i][j]))
+                            A[i][j] = 0
+                        next_x, next_y = -1, -1 
+        # print(dusk)
+        print()
+        print(t, "초 후의 상태 : ")  
+        for i in range(r):
+            for j in range(c):
+                print(A[i][j], end=" ")
+                #if A[i][j] != -1 and A[i][j] != 0 and not (i, j, A[i][j]) in dusk:
+                #    print(i, j, A[i][j], "가 안들어있음.")
+            print()
+        print()
+    return count
 
 if __name__ == "__main__":
     # r,c,t 입력
