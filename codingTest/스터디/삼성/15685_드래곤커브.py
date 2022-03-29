15e9dd47d605ff20c501fa7e9cbab0be6df08a8f
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-03-29 17:52:22 +0900

    Update 15685_드래곤커브.py

--- codingTest/스터디/삼성/15685_드래곤커브.py
+++ codingTest/스터디/삼성/15685_드래곤커브.py
@@ -15,26 +15,51 @@
 # N개의 줄, 드래곤 커브의 정보, x,y,d,g ( x,y는 드래곤 커브의 시작 점, d는 시작 방향, g는 세대) ( 0<=x,y<=100, 0<=d<=3, 0<=g<=10 )
 # 0 : x좌표가 증가하는 방향, 1: y좌표가 감소하는 방향, 2 : x좌표가 감소하는 방향, 3 : y좌표가 증가하는 방향
 
-dx = (0, -1, 0, 1) # 오, 위, 좌, 아래
-dy = (1, 0, -1, 0)
+dx = (1, 0, -1, 0) # 오, 위, 좌, 아래
+dy = (0, -1, 0, 1)
 #direction = [(1,0,3,2), (2,3,0,1), (3,2,1,0), (0,1,2,3)]
 
+def find_tangular(num, pos, check):
+    x, y = pos
+    if (x+1, y) in check[num+1:]
+
 def solution(n, dragon):
-    G_dragon = []
+    realPos=[]
     for curve in dragon:
+        G_dragon = []
         x, y, d, g = curve
-        direct = -1
-        # 첫 좌표 넣기
-        G_dragon.append((x, y, direct))
-        direct = d
+
         # 0세대 좌표 넣기
-        #G_dragon.append((x+dy[standard[direct]], y+dx[standard[direct]],direct))
+        G_dragon.append([x, y, d])
+        realPos.append((x, y))
+        pos_x, pos_y = x+dx[d], y+dy[d]
+        G_dragon.append([pos_x, pos_y, (d+1)%4])
+        if 0<=pos_x<=100 and 0<=pos_y<=100:
+            realPos.append((pos_x, pos_y))
+    
         # 다음세대 좌표 넣기
         for i in range(g):
             temp = [] # 추가되는 좌표를 위함.
-            standard = direction[direct]
+            x, y = G_dragon[-1][0], G_dragon[-1][1] # 끝점 좌표
             for j in range(len(G_dragon)-1, 0, -1): # 현재 좌표부터 최초 좌표 전까지
-                direct = G_dragon[j][2] # direct를 가져옴.
+                direct = G_dragon[j][2]
+                pos_x, pos_y = x+dx[direct], y+dy[direct]
+                temp.append([pos_x, pos_y, (direct+1)%4])
+                if 0<=pos_x<=100 and 0<=pos_y<=100:
+                    realPos.append((pos_x, pos_y))
+                x, y = pos_x, pos_y
+            G_dragon.extend(temp)
+    realPos = set(realPos) # 중복요소 제거
+    #for i in range(len(realPos)):
+    #    count = find_tangular(i, realPos[i], realPos)
+    print(realPos)
+    print(len(realPos))
+
+    # 사각형 수 구하기.
+    
+    return 0
+
+                
 if __name__ == "__main__":
     # n 입력
     n = int(input())
