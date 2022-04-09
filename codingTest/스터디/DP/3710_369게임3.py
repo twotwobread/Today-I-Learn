35be7af5ddaec69b834d1e83f4f861d357bf290f
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-04-10 08:54:36 +0900

    Update 3710_369게임3.py

--- codingTest/스터디/DP/3710_369게임3.py
+++ codingTest/스터디/DP/3710_369게임3.py
@@ -4,13 +4,11 @@
 # 숫자에 3,6,9가 들어가면 369 수에 해당됨.
 # 그때, 3,6,9가 들어간 개수 만큼 박수를 침.
 # 그외의 숫자들은 박수를 치지 않음.
-# 1 <= 
 # 시간 제한 : 2초, 메모리 제한 : 512MB
 def solution(a, b):
     result = [0] * (b+1)
     for i in range(1, b+1):
-        oneNum = i%10
-        if oneNum == 3 or oneNum == 6 or oneNum == 9:
+        if ((i%10)%3) == 0 and (i%10) != 0:
             result[i] = result[i//10] + 1
         else:
             result[i] = result[i//10]
@@ -20,4 +18,3 @@ if __name__ == "__main__":
     # a, b 입력
     a, b = map(int, input().split(" "))
     print(solution(a, b))
-# 시간초과 발생
