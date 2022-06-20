bdf3cc70f227003a92ea145ba8799ac264bbc64c
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-06-20 17:02:28 +0900

    Create 유형35_못생긴 수.py

---
+++ codingTest/다이나믹프로그래밍/유형35_못생긴 수.py
@@ -0,0 +1,25 @@
+# 문제 유형 : DP
+
+def solution():
+    dp = [0] * n
+    dp[0] = 1
+    i2, i3, i5 = 0, 0, 0
+    next2, next3, next5 = 2, 3, 5
+    for i in range(1, n):
+        dp[i] = min(next2, next3, next5)
+        if dp[i] == next2:
+            i2 += 1
+            next2 = dp[i2] * 2
+        if dp[i] == next3:
+            i3 += 1
+            next3 = dp[i3] * 3
+        if dp[i] == next5:
+            i5 += 1
+            next5 = dp[i5] * 5
+    print(dp[-1])
+
+
+
+if __name__ == "__main__":
+    n = int(input())
+    solution()
