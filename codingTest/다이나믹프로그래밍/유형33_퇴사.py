8e37adb5dfda1ea53560f64348918e1c82bdb1f0
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-06-20 17:01:06 +0900

    Update 유형33_퇴사.py

--- codingTest/다이나믹프로그래밍/유형33_퇴사.py
+++ codingTest/다이나믹프로그래밍/유형33_퇴사.py
@@ -1,36 +1,22 @@
-# 문제 유형 : 최장 증가 수열 (LCS)
-# 자료 구조 : 1차원 리스트 - 수열을 담기 위함.
+# 문제 유형 : 백트래킹, DP
+# 자료 구조 : 2차원 리스트 - 상담 일정표 저장
+#           2차원 리스트 - DP 테이블
 # [ 아이디어 ]
-# - 병사가 있는 자리마다 LCS의 길이를 계속 구해서 마지막까지 처리.
-
-def binarySearch(array, start, end, goal):
-    while start < end:
-        mid = (start + end)//2
-        if array[mid] > goal:
-            start = mid + 1
-        else:
-            end = mid
-    return end
+# - T + 일수 - 1의 날짜가 7을 안넘으면 내 날짜 이전꺼랑 더해서 비교
+# - 맨 처음 날짜의 상담은 7을 넘는지 안넘는지만 비교
 
 def solution():
-    dp = [sequence[0]]
-    for i in range(1, n):
-        if dp[-1] > sequence[i]:
-            dp.append(sequence[i])
-        else:
-            index = binarySearch(dp, 0, len(dp) - 1, sequence[i])
-            dp[index] = sequence[i]
-    print(n - len(dp))
-    print(dp)
-    # for i in range(n):
-    #     dp[i] = 1
-    #     for j in range(i):
-    #         if sequence[i] < sequence[j]:
-    #             dp[i] = max(dp[i], dp[j] + 1)
-    # print(n - max(dp))
+    dp = [0]*(n+1)
+    for i in range(1, n+1):
+        t, p = consulting[i-1]
+        if (t+i-1) <= n:
+            dp[t+i-1] = max(dp[t+i-1], dp[i-1] + p)
+        dp[i] = max(dp[i], dp[i-1])
+    print(max(dp))
 
-if __name__ =="__main__":
+
+
+if __name__ == "__main__":
     n = int(input())
-    sequence = tuple(map(int, input().split()))
-    dp = [0]*len(sequence)
+    consulting = [tuple(map(int, input().split())) for _ in range(n)]
     solution()
